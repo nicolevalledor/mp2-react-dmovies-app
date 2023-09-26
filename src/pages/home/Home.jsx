6cf5f0ec960a1514f728be9b03a80641e9f78419
Nikooolle <valledorn01@gmail.com>
2023-09-26 20:50:18 +0800

    HeroBanner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import HeroBanner from "./heroBanner/HeroBanner";
 import "./style.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <HeroBanner />
+    </div>
+  );
 };
 
 export default Home;
